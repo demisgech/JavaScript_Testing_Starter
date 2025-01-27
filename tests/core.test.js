fb38fbab0b310e73e808f56200ed6b017e3e8934
Demis Getachew <demisgech@gmail.com>
2025-01-27 23:30:33 +0300

    Test calculateDiscount function with nagative and positive testing scenario.

--- tests/core.test.js
+++ tests/core.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { getCoupons } from "../src/core.js";
+import { calculateDiscount, getCoupons } from "../src/core.js";
 describe("getCoupons", () => {
   it("should result an array of coupons", () => {
     const result = getCoupons();
@@ -24,5 +24,33 @@ describe("getCoupons", () => {
       expect(coupon.discount).toBeGreaterThan(0);
       expect(coupon.discount).toBeLessThan(1);
     });
+  });
+});
+
+describe("calculateDiscount", () => {
+  // Positive test
+  // * It ensures that out application works correctly under normal conditions.
+  // * Calling with valid input
+  it("should return discounted price if given valid code", () => {
+    expect(calculateDiscount(10, "SAVE10")).toBe(9);
+    expect(calculateDiscount(10, "SAVE20")).toBe(8);
+  });
+
+  // Negative test
+  // * It checks how well our applications handles unexpected or incorrect input.
+  // * Calling with invalid input
+  it("should handle non-numberic price", () => {
+    expect(calculateDiscount("10", "SAVE10")).toMatch(/Invalid/i);
+  });
+  it("should handle negative price", () => {
+    expect(calculateDiscount(-10, "SAVE10")).toMatch(/Invalid/i);
+  });
+
+  it("should handle non-string discount code", () => {
+    expect(calculateDiscount(10, 10)).toMatch(/invalid/i);
+  });
+
+  it("should handle invalid discount code", () => {
+    expect(calculateDiscount(10, "INVALID")).toBe(10);
   });
 });
