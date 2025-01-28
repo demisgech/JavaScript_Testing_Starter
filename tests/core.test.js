6d6f1ba6b26398d8948d147319a88eb89a939af7
Demis Getachew <demisgech@gmail.com>
2025-01-28 16:42:01 +0300

    Test user input validation.

--- tests/core.test.js
+++ tests/core.test.js
@@ -1,5 +1,9 @@
-import { describe, it, expect } from "vitest";
-import { calculateDiscount, getCoupons } from "../src/core.js";
+import { describe, it, test, expect } from "vitest";
+import {
+  calculateDiscount,
+  getCoupons,
+  validateUserInput,
+} from "../src/core.js";
 describe("getCoupons", () => {
   it("should result an array of coupons", () => {
     const result = getCoupons();
@@ -52,5 +56,36 @@ describe("calculateDiscount", () => {
 
   it("should handle invalid discount code", () => {
     expect(calculateDiscount(10, "INVALID")).toBe(10);
+  });
+});
+
+describe("validateUserInput", () => {
+  test("should return succes if given valid input", () => {
+    expect(validateUserInput("Demis", 21)).toMatch(/success/i);
+  });
+
+  test("should return an error if username is not string", () => {
+    expect(validateUserInput(123, 21)).toMatch(/Invalid/i);
+  });
+  test("should return an error if username less than 3 characters", () => {
+    expect(validateUserInput("ab", 21)).toMatch(/Invalid/i);
+  });
+  test("should return an error if username longer than 255 characters", () => {
+    expect(validateUserInput("ab".repeat(255), 21)).toMatch(/Invalid/i);
+  });
+
+  test("should return an error if age is not a number", () => {
+    expect(validateUserInput("Demis", "21")).toMatch(/Invalid/i);
+  });
+  test("should return an error if age less than 18", () => {
+    expect(validateUserInput("Demis", 17)).toMatch(/Invalid/i);
+  });
+  test("should return an error if age greater than 100", () => {
+    expect(validateUserInput("Demis", 101)).toMatch(/Invalid/i);
+  });
+
+  test("should return an error if username and age are invalid", () => {
+    expect(validateUserInput("", 0)).toMatch(/Invalid username/i);
+    expect(validateUserInput("", 0)).toMatch(/Invalid age/i);
   });
 });
