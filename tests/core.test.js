859a3eac9e8c1be0aebc8fb92c5110c9684e0251
Demis Getachew <demisgech@gmail.com>
2025-01-28 17:40:26 +0300

    Boundary testing

--- tests/core.test.js
+++ tests/core.test.js
@@ -2,6 +2,8 @@ import { describe, it, test, expect } from "vitest";
 import {
   calculateDiscount,
   getCoupons,
+  isPriceInRange,
+  isValidUsername,
   validateUserInput,
 } from "../src/core.js";
 describe("getCoupons", () => {
@@ -87,5 +89,50 @@ describe("validateUserInput", () => {
   test("should return an error if username and age are invalid", () => {
     expect(validateUserInput("", 0)).toMatch(/Invalid username/i);
     expect(validateUserInput("", 0)).toMatch(/Invalid age/i);
+  });
+});
+
+describe("isPriceInRange", () => {
+  /** ***** Boundary Testing ****
+   * A tesing techniques where we focus on the edges
+   * or boundaries of input values
+   * to ensure our software behaves correctly under extreme coditions
+   */
+  it("should return true when price is within the range", () => {
+    expect(isPriceInRange(100, 0, 150)).toBeTruthy();
+  });
+  it("should return true if price is equal to the min or the max", () => {
+    expect(isPriceInRange(100, 0, 100)).toBeTruthy();
+    expect(isPriceInRange(0, 0, 100)).toBeTruthy();
+  });
+  it("should return false when the price is outside the range", () => {
+    expect(isPriceInRange(-5, 0, 100)).toBeFalsy();
+    expect(isPriceInRange(150, 0, 100)).toBeFalsy();
+  });
+});
+
+describe("isValidUsername", () => {
+  const minLength = 5;
+  const maxLength = 15;
+  test("should return false username is too short", () => {
+    expect(isValidUsername("D".repeat(minLength - 1))).toBeFalsy();
+  });
+  test("should return false if username is too long", () => {
+    expect(isValidUsername("D".repeat(maxLength + 1))).toBeFalsy();
+  });
+
+  test("should return true when username is at the min or max Length", () => {
+    expect(isValidUsername("a".repeat(minLength))).toBeTruthy();
+    expect(isValidUsername("a".repeat(maxLength))).toBeTruthy();
+  });
+  test("should return true when username is within the length constraints", () => {
+    expect(isValidUsername("d".repeat(minLength + 1))).toBe(true);
+    expect(isValidUsername("d".repeat(maxLength - 1))).toBe(true);
+  });
+
+  test("should return false for invalid input types", () => {
+    expect(isValidUsername(undefined)).toBeFalsy();
+    expect(isValidUsername(null)).toBeFalsy();
+    expect(isValidUsername(1)).toBeFalsy();
   });
 });
