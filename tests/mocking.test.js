de213ffd401f543cc5c07597f4439520e55d276a
Demis Getachew <demisgech@gmail.com>
2025-02-24 14:23:59 +0300

    Mocking modules

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,4 +1,15 @@
-import { beforeEach, afterEach, it, expect, describe, vi } from "vitest";
+import {
+  beforeEach,
+  afterEach,
+  it,
+  expect,
+  describe,
+  vi,
+  expectTypeOf,
+} from "vitest";
+import { getExchangeRate } from "../src/libs/currency";
+import { getPriceInCurrency, getShippingInfo } from "../src/mocking";
+import { getShippingQuote } from "../src/libs/shipping";
 
 describe("test suite", () => {
   /** Mock Function
@@ -47,5 +58,38 @@ describe("test suite", () => {
     // Assert the result if ok
     expect(result).toMatch(/ok/i); // or
     expect(result).toBe("ok");
+  });
+});
+
+vi.mock("../src/libs/currency");
+describe("getPriceInCurrency", () => {
+  it("should return price in target currency", () => {
+    vi.mocked(getExchangeRate).mockReturnValue(1.5);
+    const price = getPriceInCurrency(10, "AUD");
+    expect(price).toBe(15);
+  });
+});
+
+vi.mock("../src/libs/shipping");
+describe("getShippingInfo", () => {
+  it("should return shipping unavailable if quote cannot be fetched", () => {
+    vi.mocked(getShippingQuote).mockReturnValue(null);
+    const result = getShippingInfo("US");
+
+    expect(result).toMatch(/unavailable/i);
+  });
+
+  it("should return shipping info if quote can be fetched", () => {
+    vi.mocked(getShippingQuote).mockReturnValue({
+      cost: 10,
+      estimatedDays: 3,
+    });
+
+    const result = getShippingInfo("Ethiopia");
+
+    expect(result).toMatch("$10");
+    expect(result).toMatch(/3 Days/i);
+    // Another way for the above two line of code
+    expect(result).toMatch(/Shipping cost: \$10 \(3 days\)/i);
   });
 });
